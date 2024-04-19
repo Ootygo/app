f0e48f9f183c8faa6d71be433429acfb2da308d9
ootygo <ootygo.official@gmail.com>
2024-04-19 19:56:25 +0530

    6 th commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 
 import './App.css';
 import Home from './components/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+}
+  from 'react-router-dom';
+import Places from './components/Places';
+import Stay from './components/Stay';
+import Travel from './components/Travel';
 
 
 function App() {
   return (
-    <>
-    <Home/>
-    </>
+    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Places' element={<Places />} />
+          <Route path='/Stay' element={<Stay />} />
+          <Route path='/Travel' element={<Travel />} />
+        </Routes>
+      </BrowserRouter>
+    
   );
 }
 
